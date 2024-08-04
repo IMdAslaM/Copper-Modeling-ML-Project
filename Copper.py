6366474b19e8b7da8d2d4f7fbabe760319b8f70d
Mohamed Aslam I <121491315+IMdAslaM@users.noreply.github.com>
2024-08-04 10:05:55 +0530

    Update Copper.py

--- Copper.py
+++ Copper.py
@@ -16,11 +16,19 @@ import pickle
 
 
 def page_home():
-    st.title("Welcome to My Streamlit Dashboard")
+    st.title("Welcome to My ML Application")
+    file_ = open("ML_Gif.gif", "rb")
+    contents = file_.read()
+    data_url = base64.b64encode(contents).decode("utf-8")
+    file_.close()
+    st.markdown(
+    f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+    unsafe_allow_html=True,
+)
 
     #st.markdown('## Problem Statement of this ML application:')
     st.markdown("""## <span style="color:aqua">Problem Statement of this ML application:</span>""",unsafe_allow_html=True)
-    st.markdown('The copper industry deals with less complex data related to sales and pricing. However, this data may suffer from issues such as skewness and noisy data, which can affect the accuracy of manual predictions. Dealing with these challenges manually can be time-consuming and may not result in optimal pricing decisions. A machine learning regression model can address these issues by utilizing advanced techniques such as data normalization, feature scaling, and outlier detection, and leveraging algorithms that are robust to skewed and noisy data.')
+    st.markdown('The Copper Industry deals with less complex data related to sales and pricing. However, this data may suffer from issues such as skewness and noisy data, which can affect the accuracy of manual predictions. Dealing with these challenges manually can be time-consuming and may not result in optimal pricing decisions. A machine learning regression model can address these issues by utilizing advanced techniques such as data normalization, feature scaling, and outlier detection, and leveraging algorithms that are robust to skewed and noisy data.')
     st.markdown('Another area where the copper industry faces challenges is in capturing the leads. A lead classification model is a system for evaluating and classifying leads based on how likely they are to become a customer . You can use the STATUS variable with WON being considered as Success and LOST being considered as Failure and remove data points other than WON, LOST STATUS values.')
 
 
